cd4e630159df469c31b5992f5d8091a01ccbc592
Juan Luis García López <juanluis.garcia@thoughtworks.com>
2019-12-10 00:06:55 +0100

    artist extraction and chord analyse

---
+++ src/jl_feature_extractor.py
@@ -0,0 +1,18 @@
+import pandas as pd
+
+class FeatureExtractor:
+    def extract_raw_artist(self,url):
+        plain_url = url.replace('https://tabs.ultimate-guitar.com/tab/','')
+        splitted = plain_url.split('/')
+        if len(splitted) == 1:
+            return 'Unknown'
+        
+        if len(splitted) == 2:
+            return splitted[0]
+        
+        raise Exception(f'Cannot extract artist from {url}. Splitted is {splitted}')
+        
+    def extract_pretty_artist(self,url):
+        raw_artist = self.extract_raw_artist(url)
+        raw_artist = raw_artist.replace('-','_')
+        return " ".join([word.capitalize() for word in raw_artist.split('_')])
